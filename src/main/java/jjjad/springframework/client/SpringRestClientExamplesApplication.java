28183d69060196ab9af78084a2b592ca4d4f6e9d
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-03 11:28:14 -0600

    init commit

---
+++ src/main/java/jjjad/springframework/client/SpringRestClientExamplesApplication.java
@@ -0,0 +1,13 @@
+package jjjad.springframework.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientExamplesApplication.class, args);
+	}
+
+}
